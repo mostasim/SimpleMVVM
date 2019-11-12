c4d98e97c46aa2963e4670d580e860fbea03e867
Mostasim Billah <mostasim.10@gmail.com>
2019-11-12 18:02:51 +0600

    Dagger added

---
+++ app/src/main/java/tech/mbsoft/simplemvvm/di/Test.java
@@ -0,0 +1,47 @@
+package tech.mbsoft.simplemvvm.di;
+
+import javax.inject.Inject;
+
+import dagger.Provides;
+
+public class Test {
+    public static void main(String[] args) {
+        CoffeeMaker coffeeMaker = new CoffeeMaker();
+        GreenCoffee greenCoffee = coffeeMaker.greenCoffee;
+        RedCoffee redCoffee = coffeeMaker.redCoffee;
+        greenCoffee.foo();
+        redCoffee.foo();
+    }
+
+}
+
+class CoffeeMaker{
+    @Inject public RedCoffee redCoffee;
+    @Inject public GreenCoffee greenCoffee;
+    @Provides
+    static RedCoffee provideRedCoffe() {
+        return new RedCoffee();
+    }
+    @Provides
+    static GreenCoffee provideGreenCoffe() {
+        return new GreenCoffee();
+    }
+}
+class RedCoffee{
+    @Inject
+    public RedCoffee() {
+        System.out.println("RED COFFEE");
+    }
+    public void foo(){
+        System.out.println("RED FOO!!");
+    }
+}
+class GreenCoffee{
+    @Inject
+    public GreenCoffee() {
+        System.out.println("GREEN COFFEE");
+    }
+    public void foo(){
+        System.out.println("GREEN FOO!!");
+    }
+}
